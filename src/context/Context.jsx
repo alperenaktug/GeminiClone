a970d23eef6252abe5afd32347b0427b981f1e57
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-04-19 23:10:54 +0300

    add generative AI functionality

---
+++ src/context/Context.jsx
@@ -0,0 +1,47 @@
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
+import runChat from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState([]);
+  const [prevPrompts, setPrevPrompts] = useState(false);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
+    const response = await runChat(input);
+    setResultData(response);
+    setLoading(false);
+    setInput("");
+  };
+
+  const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+  };
+
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+ContextProvider.propTypes = {
+  children: PropTypes.node,
+};
+export default ContextProvider;
